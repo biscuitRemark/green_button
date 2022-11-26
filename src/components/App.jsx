a80cc9877919d7563971f8b5907271370d834080
rumyantsev <rumyantsev@web-bee.ru>
2022-11-26 20:30:39 +0300

    added prettier polyfill babel

---
+++ src/components/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const App = () => {
+	return (
+		<div className='box'>
+			<button className='greenButton'>Green Button</button>
+		</div>
+	);
+};
+
+export default App;
--- webpack.config.js
+++ webpack.config.js
@@ -1,27 +1,43 @@
-const path = require("path");
+const path = require('path');
 
-const {CleanWebpackPlugin} = require('clean-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 const HTMLWebpackPlugin = require('html-webpack-plugin');
 
-
 module.exports = {
-    mode: "development",
-    entry: "./src/index.js",
-    output: {
-        path: path.resolve(__dirname, "dist"),
-        filename: "[name].[hash].js"
-    },
-    plugins: [
-        new HTMLWebpackPlugin({template: "./src/index.html"}),
-        new CleanWebpackPlugin()
-    ],
-    module: {
-        rules: [
-            {
-                test: /\.(css|less)$/,
-                use: ['style-loader', 'css-loader', 'less-loader']
-            }
-        ]
-    }
-
-}
+	mode: 'development',
+	entry: ['@babel/polyfill', './src/index.jsx'],
+	output: {
+		path: path.resolve(__dirname, 'dist'),
+		filename: '[name].[hash].js'
+	},
+	devServer: {
+		port: 3000
+	},
+	plugins: [
+		new HTMLWebpackPlugin({ template: './src/index.html' }),
+		new CleanWebpackPlugin()
+	],
+	module: {
+		rules: [
+			{
+				test: /\.(css|less)$/,
+				use: ['style-loader', 'css-loader', 'less-loader']
+			},
+			{ test: /\.(jpg|jped|png|svg)$/, use: ['file-loader'] },
+			{
+				test: /\.js$/,
+				exclude: /node_modules/,
+				loader: 'babel-loader',
+				options: { presets: ['@babel/preset-env'] }
+			},
+			{
+				test: /\.jsx$/,
+				exclude: /node_modules/,
+				loader: 'babel-loader',
+				options: {
+					presets: ['@babel/preset-react', '@babel/preset-env']
+				}
+			}
+		]
+	}
+};
